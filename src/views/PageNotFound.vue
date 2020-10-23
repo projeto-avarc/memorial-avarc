ef82f623a3110a39abe15bfedd6ae9bd73dfee18
Victor Nascimento <victor.nascimento.ns@gmail.com>
2020-10-22 21:10:46 -0300

    PageNotFound

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      
+    <h1 class="text-2xl mb-5 mt-20">Página não encontrada :(</h1>
+    <router-link to="/">
+      <Button value="Voltar" class="primary-color text-sm"></Button>
+    </router-link>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Button from '@/components/Button.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Button
+  }
+}
+</script>
+
+<style lang="scss">
+.home {
+  > p {
+    margin-bottom: 20px;
+  }
+}
+</style>
