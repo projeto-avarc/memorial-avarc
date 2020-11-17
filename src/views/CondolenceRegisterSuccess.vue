208c44e68adf3b9feecaafecf1f6431acb9a094d
Victor Nascimento <victor.nascimento.ns@gmail.com>
2020-11-16 21:33:40 -0300

    styled component success condolence

--- src/views/CondolenceRegisterSuccess.vue
+++ src/views/CondolenceRegisterSuccess.vue
@@ -1,35 +1,50 @@
 <template>
-  <div>
-    <p class="info">Sua condolência foi registrada e está em processo de aprovação pelos moderadores. Em breve você receberá um e-mail com seu QR Code para compartilhar com os amigos!</p>
+  <div class="modal-success" v-show="modal">
+    <div class="box">
+      <p>Sua condolência foi registrada e está em processo de aprovação pelos moderadores.</p>
+      <p>Em breve você receberá um e-mail com seu QR Code para compartilhar com os amigos!</p>
 
-    <div class="mt-12 d-flex">
-      <router-link to="/condolencia/criar" class="mx-1 mb-2 inline-block text-sm">
-        <Button value="Escrever nova condolência"></Button>
-      </router-link>
-      <router-link to="/condolencias" class="mx-1 mb-2 inline-block text-sm">
-        <Button value="Ver condolências" class="primary-color"></Button>
-      </router-link>
+      <button @click="modal = false" class="mt-5 bg-transparent hover:bg-red-400 text-red-400 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded">Fechar</button>
     </div>
   </div>
 </template>
 
 <script>
-import Button from '@/components/Button.vue'
-
 export default {
   name: 'qrCode',
-  components: {
-    Button
-  },
   data () {
-    return {}
+    return {
+      modal: true,
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  .info {
-    @apply mb-16 mt-20 mx-auto font-bold;
-    max-width: 680px;
+  .modal-success {
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(0,0,0,0.8);
+    z-index: 10;
+    position: fixed;
+    top: 0;
+    left: 0;
+
+    .box {
+      @apply bg-white px-12 py-16 absolute shadow-2xl rounded inline-block text-center;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+      width: 100%;
+      max-width: 800px;
+
+      @media (max-width: 850px) {
+        max-width: 80%;
+      }
+    }
+
+    p {
+      @apply m-2 mx-auto block text-xl;
+    }
   }
 </style>
