fcd1566373d2d3c2770f14a165b2b14494933480
Victor Nascimento <victor.nascimento.ns@gmail.com>
2020-11-25 17:00:32 -0300

    created component jumbotron

---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,23 @@
+<template v-slot:content>
+  <div>
+      jumbotron
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InformationModal",
+  data() {
+    return {
+      
+    };
+  },
+  props: {
+    person: Object,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
