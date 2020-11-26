a4cd44e77f652d8e3672f41bd16ca730f79d3532
Victor Nascimento <victor.nascimento.ns@gmail.com>
2020-11-26 04:21:47 -0300

    styled responsive component infoHero

--- src/components/InfoHero.vue
+++ src/components/InfoHero.vue
@@ -1,26 +1,28 @@
 <template>
-  <div class="title my-10 md:mt-16 md:mb-20">
-    <p
-      class="text-base md:text-2xl lg:text-4xl xl:text-5xl sm:text-center lg:text-left text-blue-900"
-    >
-      Memorial às vítimas
-    </p>
-    <p
-      class="leading-7 text-base font-bold md:text-2xl lg:text-4xl xl:text-5xl sm:text-center lg:text-left text-blue-900"
-    >
-      do Covid-19
-    </p>
-    <p class="text-base md:text-lg sm:text-justify lg:text-left text-blue-900 mt-10">
-      Deixe uma mensagem para <br />
-      um amigo ou ente querido, <br />
-      e eternize esse sentimento.
-    </p>
-    <router-link to="/condolencia/criar">
-      <Button
-        value="Prestar condolência"
-        class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full mt-4"
-      ></Button>
-    </router-link>
+  <div class="flex justify-center md:block">
+    <div class="title my-10 md:mt-16 inline-block bg-white p-8 md:p-0 md:pl-2 block relative shadow-2xl rounded md:bg-transparent md:shadow-none">
+      <p
+        class="text-2xl lg:text-4xl xl:text-5xl text-blue-900 text-left"
+      >
+        Memorial às vítimas
+      </p>
+      <p
+        class="leading-7 font-bold text-2xl lg:text-4xl xl:text-5xl text-left text-blue-900"
+      >
+        do Covid-19
+      </p>
+      <p class="font-semibold text-base md:text-xl text-left text-blue-900 md:font-normal md:mt-10 mt-4">
+        Deixe uma mensagem para <br />
+        um amigo ou ente querido, <br />
+        e eternize esse sentimento.
+      </p>
+      <router-link to="/condolencia/criar">
+        <Button
+          value="Prestar condolência"
+          class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full mt-4"
+        ></Button>
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -33,3 +35,6 @@ export default {
   },
 };
 </script>
+
+<style scoped lang="scss">
+</style>
