7c9c3e4736f95197ce794eef9817dad55d1179d0
Victor Nascimento <victor.nascimento.ns@gmail.com>
2021-01-04 15:24:27 -0300

    pagination import

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import axios from "axios"
 import Vuelidate from "vuelidate"
 import { VueMaskDirective } from "v-mask"
 import VueSocialSharing from 'vue-social-sharing'
+import Paginate from 'vuejs-paginate'
 
 // css imports
 import "@/assets/css/tailwind.scss"
@@ -16,6 +17,7 @@ Vue.config.productionTip = false
 Vue.use(Vuelidate)
 Vue.directive("mask", VueMaskDirective)
 Vue.use(VueSocialSharing)
+Vue.component('paginate', Paginate)
 
 new Vue({
   router,
